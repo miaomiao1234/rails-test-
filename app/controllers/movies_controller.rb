4aeb2014219c42ca7d26694e6ea3e3fed1e83666
miaoliang <miaoliang01@gmail.com>
2017-04-07 16:25:57 +0800

    implement movies#show

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,8 +4,25 @@ class MoviesController < ApplicationController
     @movies = Movie.all
   end
 
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
   def new
     @movie = Movie.new
+end
+
+def create
+  @movie = Movie.new(movie_params)
+  @movie.save
+
+  redirect_to movies_path
+end
+
+private
+
+def movie_params
+  params.require(:movie).permit(:title, :decription)
 end
 
 end
